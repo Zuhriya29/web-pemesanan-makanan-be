0c2ad03c1fcc672a7a7eb80d5f069546931bf180
Zuhriya29 <113580042+Zuhriya29@users.noreply.github.com>
2026-05-28 15:02:02 +0700

    edit file

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,7 +10,7 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:5173')
+            ->header('Access-Control-Allow-Origin', 'https://griya-dhahar-suroboyo.vercel.app')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
             ->header('Access-Control-Allow-Credentials', 'true');
